a20d21ad4330a596af8f94132be4e32aa902dad1
lord <marialindananono@gmail.com>
2020-12-03 07:15:11 +0300

    changing port

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("/search/:imdbDB", (req, res) => {
   });
 });
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.info(`Server listen on port ${PORT}`);
